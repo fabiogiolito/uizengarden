f2394e8a6f8dd788316c9a89cd5bc6da1949f927
fabiogiolito <fabiogiolito@gmail.com>
2022-08-12 11:34:45 +0100

    ToggleGroup component

---
+++ src/lib/ToggleGroup.svelte
@@ -0,0 +1,16 @@
+<script>
+
+  export let current = null;
+
+  function setCurrent(val) {
+    console.log(val);
+    if (val?.detail) {
+      current = val.detail;
+    } else {
+      current = val;
+    }
+  }
+
+</script>
+
+<slot {current} {setCurrent} />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { slide } from "svelte/transition";
+
   import Button from "$lib/Button.svelte";
   import Dropdown from "$lib/Dropdown.svelte";
   import Collapsible from "$lib/Collapsible.svelte";
@@ -8,6 +10,7 @@
   import Select from "$lib/Select.svelte";
   import Checkbox from "$lib/Checkbox.svelte";
   import Tooltip from "$lib/Tooltip.svelte";
+  import ToggleGroup from "$lib/ToggleGroup.svelte";
 
   import IconCamera from "$lib/icons/IconCamera.svelte";
   import IconCopy from "$lib/icons/IconCopy.svelte";
@@ -21,6 +24,8 @@
 
   let modalOpen = false;
   let modalRef;
+
+  let currentTab = 1;
 
   function handleOpenModal() {
     modalOpen = true;
@@ -79,7 +84,7 @@
 
     <div class="uizg-previewArea">
       <div class="uizg-previewElement">
-        <Button>button</Button>
+        <Button on:click={() => { alert('clicked')}}>button</Button>
         <Button href="#link">link</Button>
         <Button element="span">span</Button>
       </div>
@@ -197,14 +202,20 @@
     <div class="uizg-previewArea">
       <div class="uizg-previewElement">
         <Button toggle>Toggle</Button>
-        <Button toggle let:selected>
+        <Button toggle let:selected
+          on:select={() => console.log('did select')}
+          on:unselect={() => console.log('did unselect')}
+        >
           {selected ? 'Following' : 'Follow'}
         </Button>
       </div>
       <div class="uizg-previewCode">
         {`
           <Button toggle>Toggle</Button>
-          <Button toggle let:selected>
+          <Button toggle let:selected
+            on:select={() => console.log('did select')}
+            on:unselect={() => console.log('did unselect')}
+          >
             {selected ? 'Following' : 'Follow'}
           </Button>
         `}
@@ -489,7 +500,9 @@
         <Collapsible collapsed let:collapsed let:toggleCollapsed>
           <div slot="trigger">
             {#if collapsed}
-              <Button size="xs" on:click={toggleCollapsed}>2 more</Button>
+              <div transition:slide>
+                <Button size="xs" on:click={toggleCollapsed}>2 more</Button>
+              </div>
             {/if}
           </div>
           <p>Hello one</p>
@@ -501,7 +514,9 @@
           <Collapsible collapsed let:collapsed let:toggleCollapsed>
             <div slot="trigger">
               {#if collapsed}
-                <Button size="xs" on:click={toggleCollapsed}>2 more</Button>
+                <div transition:slide>
+                  <Button size="xs" on:click={toggleCollapsed}>2 more</Button>
+                </div>
               {/if}
             </div>
             <p>Hello one</p>
@@ -1047,6 +1062,120 @@
             <Button>Horizontal align right</Button>
             <div slot="content">...</div>
           </Tooltip>
+        `}
+      </div>
+    </div>
+
+  </div>
+
+  <!-- ============================== -->
+  <!-- ToggleGroup -->
+  <div class="uizg-component">
+
+    <h2>ToggleGroup</h2>
+
+    <div class="uizg-previewArea">
+      <div class="uizg-previewElement">
+        <ToggleGroup let:current let:setCurrent>
+          <p on:click={() => setCurrent(1)}>Item 1 {current == 1 ? '(selected)' : ''}</p>
+          <p on:click={() => setCurrent(2)}>Item 2 {current == 2 ? '(selected)' : ''}</p>
+          <p on:click={() => setCurrent(3)}>Item 3 {current == 3 ? '(selected)' : ''}</p>
+        </ToggleGroup>
+      </div>
+      <div class="uizg-previewCode">
+        {`
+          <ToggleGroup let:current let:setCurrent>
+            <p on:click={() => setCurrent(1)}>Item 1 {current == 1 ? '(selected)' : ''}</p>
+            <p on:click={() => setCurrent(2)}>Item 2 {current == 2 ? '(selected)' : ''}</p>
+            <p on:click={() => setCurrent(3)}>Item 3 {current == 3 ? '(selected)' : ''}</p>
+          </ToggleGroup>
+        `}
+      </div>
+    </div>
+
+    <div class="uizg-previewArea">
+      <div class="uizg-previewElement">
+        <ToggleGroup current={2} let:current let:setCurrent>
+          <Button value={1} selected={current == 1} on:click={setCurrent}>Tab 1</Button>
+          <Button value={2} selected={current == 2} on:click={setCurrent}>Tab 2</Button>
+          <Button value={3} selected={current == 3} on:click={setCurrent}>Tab 3</Button>
+
+          <!-- Content logic here -->
+          <p>Current Tab is {current}</p>
+        </ToggleGroup>
+      </div>
+      <div class="uizg-previewCode">
+        {`
+          <ToggleGroup current={2} let:current let:setCurrent>
+            <Button value={1} selected={current == 1} on:click={setCurrent}>Tab 1</Button>
+            <Button value={2} selected={current == 2} on:click={setCurrent}>Tab 2</Button>
+            <Button value={3} selected={current == 3} on:click={setCurrent}>Tab 3</Button>
+
+            <!-- Content logic here -->
+            <p>Current Tab is {current}</p>
+          </ToggleGroup>
+        `}
+      </div>
+    </div>
+
+    <div class="uizg-previewArea">
+      <div class="uizg-previewElement">
+        <ToggleGroup bind:current={currentTab} let:current let:setCurrent>
+          <Button value={1} selected={current == 1} on:click={setCurrent}>Tab 1</Button>
+          <Button value={2} selected={current == 2} on:click={setCurrent}>Tab 2</Button>
+          <Button value={3} selected={current == 3} on:click={setCurrent}>Tab 3</Button>
+        </ToggleGroup>
+        <p>Current Tab with external state is {currentTab}</p>
+      </div>
+      <div class="uizg-previewCode">
+        {`
+          <script>
+            let currentTab = 1;
+          </script>
+          <ToggleGroup bind:current={currentTab} let:current let:setCurrent>
+            <Button value={1} selected={current == 1} on:click={setCurrent}>Tab 1</Button>
+            <Button value={2} selected={current == 2} on:click={setCurrent}>Tab 2</Button>
+            <Button value={3} selected={current == 3} on:click={setCurrent}>Tab 3</Button>
+          </ToggleGroup>
+          <p>Current Tab is {currentTab}</p>
+        `}
+      </div>
+    </div>
+
+    <div class="uizg-previewArea">
+      <div class="uizg-previewElement">
+        <ToggleGroup current={'Item 2'} let:current let:setCurrent>
+          {#each ['Item 1', 'Item 2', 'Item 3'] as option}
+            <div><Checkbox value={option} checked={current == option} on:check={setCurrent} label={option} /></div>
+          {/each}
+        </ToggleGroup>
+      </div>
+      <div class="uizg-previewCode">
+        {`
+          <ToggleGroup current={'Item 2'} let:current let:setCurrent>
+            {#each ['Item 1', 'Item 2', 'Item 3'] as option}
+              <div><Checkbox value={option} checked={current == option} on:check={setCurrent} label={option} /></div>
+            {/each}
+          </ToggleGroup>
+        `}
+      </div>
+    </div>
+
+    <div class="uizg-previewArea">
+      <div class="uizg-previewElement">
+        <ToggleGroup current={1} let:current let:setCurrent>
+          <Collapsible value={1} collapsed={current != 1} on:expand={setCurrent} title="Item 1">Hello World!</Collapsible>
+          <Collapsible value={2} collapsed={current != 2} on:expand={setCurrent} title="Item 2">Hello World!</Collapsible>
+          <Collapsible value={3} collapsed={current != 3} on:expand={setCurrent} title="Item 3">Hello World!</Collapsible>
+        </ToggleGroup>
+      </div>
+      <div class="uizg-previewCode">
+        {`
+        <ToggleGroup current={1} let:current let:setCurrent>
+          <Collapsible value={1} collapsed={current != 1} on:expand={setCurrent} title="Item 1">...</Collapsible>
+          <Collapsible value={2} collapsed={current != 2} on:expand={setCurrent} title="Item 2">...</Collapsible>
+          <Collapsible value={3} collapsed={current != 3} on:expand={setCurrent} title="Item 3">...</Collapsible>
+        </ToggleGroup>
         `}
       </div>
     </div>
