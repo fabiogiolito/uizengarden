fcecc1fb69c115c8c4e59c9f627c2aeb25d63fcd
fabiogiolito <fabiogiolito@gmail.com>
2022-08-14 10:53:51 +0100

    Modal header/footer and dialog

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -1,12 +1,27 @@
 <script>
+  import { createEventDispatcher } from "svelte";
+
   import { fade, fly } from "svelte/transition"
   import { teleport } from "$lib/helpers/teleport";
 
+  import IconX from "$lib/icons/IconX.svelte";
+
+  const dispatch = createEventDispatcher();
+
   export let open = false;
 
+  export let dialog = false; // Not closable unless you interact
+  export let title = ""; // Header with title
+
   export let classBase = "modal";
+  export let classDialog  = `${classBase}--dialog`;
   export let classOverlay = `${classBase}__overlay`;
   export let classWindow  = `${classBase}__window`;
+  export let classHeader  = `${classBase}__header`;
+  export let classTitle   = `${classBase}__title`;
+  export let classClose   = `${classBase}__close`;
+  export let classContent = `${classBase}__content`;
+  export let classFooter  = `${classBase}__footer`;
 
   let className = "";
   export { className as class };
@@ -17,18 +32,23 @@
 
   export function openModal() {
     open = true;
+    dispatch('open');
   }
 
   export function closeModal() {
     open = false;
+    dispatch('close');
   }
 
   function handleKeydown(e) {
     if (!open) return; // Ignore if not open
+
     if (e.key === "Escape") {
+      if (dialog) return; // Don't close dialog with Escape
       e.stopPropagation();
       closeModal(); // Close on ESC
     }
+
   }
 </script>
 
@@ -38,21 +58,58 @@
 
 {#if open}
 
-  <div use:teleport class="{classBase} {className}">
+  <!-- Fullscreen container -->
+  <div use:teleport class="{classBase} {dialog ? classDialog : ''} {className}">
 
-    <!-- Close modal overlay -->
-    <button
-      on:click={closeModal}
-      transition:fade={{ duration: 200 }}
-      class={classOverlay}
-    />
+    <!-- Fullscreen overlay - Close modal -->
+    {#if dialog}
+      <div transition:fade={{ duration: 200 }} class={classOverlay} />
+    {:else}
+      <button
+        on:click={closeModal}
+        transition:fade={{ duration: 200 }}
+        class={classOverlay}
+      />
+    {/if}
 
     <!-- Modal window -->
     <div
       transition:fly={{ y: 40, duration: 200 }}
       class={classWindow}
     >
-      <slot {closeModal} />
+
+      <!-- Header -->
+      <slot name="header" {closeModal}>
+        {#if title}
+          <div class={classHeader}>
+            <div class={classTitle}>
+              {title}
+            </div>
+            {#if !dialog}
+              <button
+                on:click={closeModal}
+                transition:fade={{ duration: 200 }}
+                class={classClose}
+              >
+                <IconX />
+              </button>
+            {/if}
+          </div>
+        {/if}
+      </slot>
+
+      <!-- Content -->
+      <div class={classContent}>
+        <slot {closeModal} />
+      </div>
+
+      <!-- Footer -->
+      {#if $$slots.footer}
+        <div class={classFooter}>
+          <slot name="footer" {closeModal} />
+        </div>
+      {/if}
+
     </div>
 
   </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -645,6 +645,79 @@
       </div>
     </div>
 
+    <div class="uizg-previewArea">
+      <div class="uizg-previewElement">
+        <Modal title="My modal" let:openModal >
+          <Button slot="trigger" on:click={openModal}>
+            Modal with title
+          </Button>
+          <p>Content</p>
+        </Modal>
+      </div>
+      <div class="uizg-previewCode">
+        {`
+          <Modal title="My modal" let:openModal >
+            <Button slot="trigger" on:click={openModal}>
+              Modal with title
+            </Button>
+            <p>Content</p>
+          </Modal>
+        `}
+      </div>
+    </div>
+
+    <div class="uizg-previewArea">
+      <div class="uizg-previewElement">
+        <Modal let:openModal let:closeModal>
+          <Button slot="trigger" on:click={openModal}>
+            Modal with header and footer
+          </Button>
+          <div slot="header">My modal</div>
+          <p>Content</p>
+          <Button slot="footer" on:click={closeModal}>Close modal</Button>
+        </Modal>
+      </div>
+      <div class="uizg-previewCode">
+        {`
+          <Modal let:openModal let:closeModal>
+            <Button slot="trigger" on:click={openModal}>
+              Modal with header and footer
+            </Button>
+            <div slot="header">My modal</div>
+            <p>Content</p>
+            <Button slot="footer" on:click={closeModal}>Close modal</Button>
+          </Modal>
+        `}
+      </div>
+    </div>
+
+    <div class="uizg-previewArea">
+      <div class="uizg-previewElement">
+        <Modal dialog title="Are you sure?" let:openModal let:closeModal>
+          <Button slot="trigger" on:click={openModal}>
+            Modal dialog
+          </Button>
+          <div slot="footer">
+            <Button on:click={() => { console.log('action'); closeModal(); }}>Yes</Button>
+            <Button on:click={closeModal}>No</Button>
+          </div>
+        </Modal>
+      </div>
+      <div class="uizg-previewCode">
+        {`
+          <Modal dialog title="Are you sure?" let:openModal let:closeModal>
+            <Button slot="trigger" on:click={openModal}>
+              Modal dialog
+            </Button>
+            <div slot="footer">
+              <Button on:click={() => { console.log('action'); closeModal(); }}>Yes</Button>
+              <Button on:click={closeModal}>No</Button>
+            </div>
+          </Modal>
+        `}
+      </div>
+    </div>
+
   </div>
 
 
